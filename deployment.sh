2d9def8f7efb3fc023b02ef50cafdb8812a7333c
inc-abhijeet <51852905+inc-abhijeet@users.noreply.github.com>
2020-03-06 19:11:41 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,2 +1,2 @@
  #!/bin/bash
- sudo git -C  /root/devops reset  --hard && sudo git -C  /root/devops pull https://github.com/incorganisation/devops && crontab /root/devops/crontabs/crontab_1.py && sudo chmod +x ./devops/scripts/script_1.sh && sudo ./devops/scripts/script_1.sh
+ sudo git -C  /root/devops reset  --hard && sudo git -C  /root/devops pull https://github.com/incorganisation/devops && crontab /root/devops/crontabs/crontab_1.py
